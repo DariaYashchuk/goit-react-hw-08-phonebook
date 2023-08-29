f2214cb3560541edb3697dea9526af463cad9f66
DariaYashchuk <121041236+DariaYashchuk@users.noreply.github.com>
2023-08-29 18:43:57 +0000

    Deploying to gh-pages from @ DariaYashchuk/goit-react-hw-08-phonebook@15a38b23ec1cc0f0156f2e77b8a0060c77f27943 🚀

---
+++ static/js/692.87b417a1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[692],{7692:function(t,a,h){h.d(a,{SW4:function(){return r}});var e=h(9983);function r(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"}},{tag:"path",attr:{d:"M9 10h2v8H9zm4 0h2v8h-2z"}}]})(t)}}}]);
+//# sourceMappingURL=692.87b417a1.chunk.js.map
